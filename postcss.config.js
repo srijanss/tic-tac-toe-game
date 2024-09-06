b385a46f359b22a84f06f0ec81f51bb5c48f817b
Srijan Manandhar <srijan.manandhar@gmail.com>
2024-09-06 14:10:07 +0545

    Added initial code

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: [
+    require("autoprefixer")({
+      overrideBrowserslist: ["last 2 versions", "ie > 8"],
+    }),
+    require("postcss-preset-env")({
+      stage: 1,
+    }),
+    require("postcss-import"),
+    require("postcss-nested"),
+    require("postcss-mixins"),
+    require("cssnano")({
+      preset: "default",
+    }),
+  ],
+};
---
+++ vite.config.js
@@ -0,0 +1,3 @@
+export default {
+  base: "/tic-tac-toe-game",
+};
