0e229efe9fadb6d91712f1fb4b67de52600f5766
Srijan Manandhar <srijan.manandhar@gmail.com>
2024-09-10 06:45:43 +0545

    Refactor player score display in FooterComponent

--- assets/js/components/footer/FooterComponent.js
+++ assets/js/components/footer/FooterComponent.js
@@ -4,6 +4,14 @@ import Store from "../../store";
 export default class FooterComponent extends HTMLElement {
   constructor() {
     super();
+    this.playerWithXMark =
+      Store.player1Mark === "x" ? Store.player1 : Store.player2;
+    this.playerWithOMark =
+      Store.player1Mark === "o" ? Store.player1 : Store.player2;
+    this.XMarkScore =
+      Store.player1Mark === "x" ? Store.player1Score : Store.player2Score;
+    this.OMarkScore =
+      Store.player1Mark === "o" ? Store.player1Score : Store.player2Score;
   }
 
   connectedCallback() {
@@ -16,16 +24,16 @@ export default class FooterComponent extends HTMLElement {
       <style>${css}</style>
       <footer>
         <section class="score-component x-score">
-          <h2>${Store.player1Mark} (${Store.player1})</h2>
-          <p>${Store.player1Score}</p>
+          <h2>X (${this.playerWithXMark})</h2>
+          <p>${this.XMarkScore}</p>
         </section>
         <section class="score-component tie-score">
           <h2>Ties</h2>
           <p>${Store.ties}</p>
         </section>
         <section class="score-component o-score">
-          <h2>${Store.player2Mark} (${Store.player2})</h2>
-          <p>${Store.player2Score}</p>
+          <h2>O (${this.playerWithOMark})</h2>
+          <p>${this.OMarkScore}</p>
         </section>
       </footer>
     `;
